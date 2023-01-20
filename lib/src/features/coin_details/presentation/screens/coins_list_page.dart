2c53f736e4603014a5d18a1a3b1af85f645a040d
Oghenamaro Afenogho <maroafenogho@gmail.com>
2023-01-20 23:56:19 +0100

    refresh

--- lib/src/features/coin_details/presentation/screens/coins_list_page.dart
+++ lib/src/features/coin_details/presentation/screens/coins_list_page.dart
@@ -8,109 +8,123 @@ class CoinsHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Consumer(builder: (((context, ref, child) {
-          final asyncCoins = ref.watch(asyncCoinProvider);
+      body: Consumer(builder: (((context, ref, child) {
+        final asyncCoins = ref.watch(asyncCoinProvider);
 
-          return asyncCoins.when(
-              data: (coin) {
-                // Timer.periodic(const Duration(seconds: 2), (timer) {
-                //   ref.refresh(coinStreamProvider);
-                // });
-                return SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: ListView.builder(
-                    itemCount: coin.length,
-                    itemBuilder: (context, index) => Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              ref.read(selectedCoin.notifier).state =
-                                  coin[index];
-                              print(ref.watch(selectedCoin).name);
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        return asyncCoins.when(
+            data: (coin) {
+              // Timer.periodic(const Duration(seconds: 2), (timer) {
+              //   ref.refresh(coinStreamProvider);
+              // });
+              return SingleChildScrollView(
+                child: RefreshIndicator(
+                  onRefresh: () =>
+                      ref.watch(asyncCoinProvider.notifier).getCoins(),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.9,
+                        child: ListView.builder(
+                          itemCount: coin.length,
+                          itemBuilder: (context, index) => Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Column(
                               children: [
-                                Expanded(
-                                  flex: 2,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    ref.read(selectedCoin.notifier).state =
+                                        coin[index];
+                                    print(ref.watch(selectedCoin).name);
+                                  },
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text(
-                                        coin[index].symbol.toUpperCase(),
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 18,
+                                      Expanded(
+                                        flex: 2,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              coin[index].symbol.toUpperCase(),
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 18,
+                                              ),
+                                            ),
+                                            Text(
+                                              coin[index].name,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.w300,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                      Text(
-                                        coin[index].name,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.w300,
-                                          fontSize: 12,
+                                      Expanded(
+                                        flex: 1,
+                                        child: Text(
+                                          coin[index].priceChangePercent > 0
+                                              ? '+${coin[index].priceChangePercent.toStringAsFixed(2)}%'
+                                              : '${coin[index].priceChangePercent.toStringAsFixed(2)}%',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            color:
+                                                coin[index].priceChangePercent >
+                                                        0
+                                                    ? Colors.green
+                                                    : Colors.red,
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Text(
+                                          '\$${coin[index].currentPrice}',
+                                          textAlign: TextAlign.end,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 18,
+                                          ),
                                         ),
                                       ),
                                     ],
                                   ),
                                 ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    coin[index].priceChangePercent > 0
-                                        ? '+${coin[index].priceChangePercent.toStringAsFixed(2)}%'
-                                        : '${coin[index].priceChangePercent.toStringAsFixed(2)}%',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 14,
-                                      color: coin[index].priceChangePercent > 0
-                                          ? Colors.green
-                                          : Colors.red,
-                                    ),
-                                  ),
+                                const Divider(
+                                  color: Colors.black54,
+                                  height: 2,
                                 ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    '\$${coin[index].currentPrice}',
-                                    textAlign: TextAlign.end,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 18,
-                                    ),
-                                  ),
+                                const SizedBox(
+                                  height: 10,
                                 ),
                               ],
                             ),
                           ),
-                          const Divider(
-                            color: Colors.black54,
-                            height: 2,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                );
-              },
-              error: (error, stacktrace) {
-                print(stacktrace);
-                return const Text('error');
-              },
-              loading: () => SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: const Center(child: CircularProgressIndicator())));
-        })))
-      ]),
+                ),
+              );
+            },
+            error: (error, stacktrace) {
+              print(stacktrace);
+              return const Text('error');
+            },
+            loading: () => SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: const Center(child: CircularProgressIndicator())));
+      }))),
     );
   }
 }
